1e43ed3b0e93b0651bc49d62a13a1f56dc96b5d8
Saud-Khan-Dev <asaud3508@gmail.com~>
2025-07-19 22:04:08 +0500

    completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import Tesseract from 'tesseract.js';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
-  protected title = 'extract_text_from_image';
+  title = 'extract_text_from_image';
+
+  extracted_text = signal<string>('');
+  error_message = signal<string>('');
+  isLoading=signal<boolean>(false)
+
+  onImageUpload(event: any) {
+    const file = event.target?.files[0];
+   
+    if (file) {
+       this.isLoading.set(true);
+      const reader = new FileReader();
+      reader.onload = () => {
+        const imageData = reader.result;
+        if (imageData) {
+          this.performOCR(imageData);
+        } else {
+         this.isLoading.set(false);
+          this.error_message.set('Could not read image data');
+        }
+      };
+      reader.onerror = () => {
+        this.isLoading.set(false); 
+        this.error_message.set('Error reading file');
+      };
+      reader.readAsDataURL(file);
+    } else {
+      this.error_message.set('No file selected');
+    }
+  }
+
+  performOCR(imageData: any) {
+    Tesseract.recognize(imageData)
+      .then((result:any) => {
+       
+        this.extracted_text.set(result.data.text);
+         this.isLoading.set(false);
+      })
+      .catch((error:any) => {
+         this.isLoading.set(false);
+        this.error_message.set('OCR failed. Please try another image.');
+      });
+  }
 }
